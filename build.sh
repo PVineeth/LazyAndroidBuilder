d945730f0c2bcb21b684ce17e9f64977c53f285c
PVineeth <vineeth.pen@outlook.com>
2018-11-04 20:21:53 -0600

    Updated to v0.2
    More robust than v0.2
    
    - Added newer reset, update functions.
    - Solved "first_run()" function bug: It used to come up even if "help" argument was passed on to the script.

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# LazyAndroidBuilder v0.1
+# LazyAndroidBuilder v0.2
 # Script For Building Android Oreo
 # Author: Vineeth Penugonda
-# License: GPL v3
+# License: AGPL v3
 
-#Print Software Version
-echo -e "\nLazyAndroidBuilder v0.1\n"
+# Print Software Version
+echo -e "\nLazyAndroidBuilder v0.2"
+echo -e "Author: Vineeth Penugonda\n"
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 
 # store arguments in a special array 
@@ -17,6 +18,11 @@ ELEMENTS=${#args[@]}
 # Global Var
 SCRIPT_DIR=$( cd ${0%/*} && pwd -P )
 
+# Colors
+red=$'\e[1;31m' # Errors
+mag=$'\e[1;35m' # Success
+end=$'\e[0m'
+
 # Functions
 
 clean () {
@@ -24,18 +30,44 @@ clean () {
 }
 
 changedir () {
+    # Change shell directory
     cd $DIRECTORYPATH
 }
 
 printVar () {
     echo "SCRIPT_DIR: $SCRIPT_DIR"
     echo "DIRECTORYPATH: $DIRECTORYPATH"
+    echo "DEVICE_NAME: $DEVICE_NAME"
 }
 
 clrscr () {
     printf "\033c"
 }
 
+set_env() {
+# Globally Required
+export ANDROID_JACK_VM_ARGS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4G"
+export LC_ALL=C
+export ALLOW_MISSING_DEPENDENCIES=true
+# export KBUILD_BUILD_USER=vineethp-linuxninja
+# export KBUILD_BUILD_HOST=vineethp-linux-ninja
+}
+
+device_name() {
+if [ ! -f .devicename.dat ]; then
+    echo "Enter device codename (usually 'ro.product.device' in build.prop):"
+    read DEVICE_NAME
+    if [[ ! -z "$DEVICE_NAME" ]]; then
+        echo $DEVICE_NAME > .devicename.dat
+    else 
+        echo "Please enter the device codename to continue. It cannot be empty!"
+    fi
+else
+    DEVICE_NAME=$(<.devicename.dat)
+fi
+}
+
+first_run() {
 # FirstRun
 if [ ! -f .firstrun.dat ]; then 
     echo "Enter the directory path to the source code and press [ENTER]:"
@@ -43,26 +75,55 @@ if [ ! -f .firstrun.dat ]; then
     read DIRECTORYPATH
     if [[ ! -z "$DIRECTORYPATH" ]]; then
         echo $DIRECTORYPATH > .firstrun.dat
+        device_name
     else 
         echo "Please enter the directory path to continue. It cannot be empty!"
     fi
 else 
     DIRECTORYPATH=$(<.firstrun.dat)
+    device_name
     # Print Variable Values
     printVar
     printf '%*s\n\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 fi
+}
 
-# Change shell directory
-cd $DIRECTORYPATH
+reset() {
+    if [[ -f .firstrun.dat || -f .devicename.dat ]]; then
+        cd $SCRIPT_DIR
+        rm -f ".firstrun.dat"
+        rm -f ".devicename.dat"
+        clrscr
+        printf "%s\n" "${mag}Done resetting the script!${end}"
+        exit 0 # 0 = successfully
+    else 
+        clrscr
+        printf "%s\n" "${red}The LazyAndroidBuilder did not even run once. No use of resetting now.${end}"
+        exit 1 # 1 = unsuccessfully
+    fi
+}
 
-# Globally Required
-export ANDROID_JACK_VM_ARGS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4G"
-export LC_ALL=C
-export ALLOW_MISSING_DEPENDENCIES=true
-# export KBUILD_BUILD_USER=vineethp-linuxninja
-# export KBUILD_BUILD_HOST=vineethp-linux-ninja
+update_script(){
+    rm -rf .tmp_labscript.sh
+    echo "LazyAndroidBuilder Updater"
+    wget --output-document=.tmp_labscript.sh -q https://raw.githubusercontent.com/PVineeth/LazyAndroidBuilder/master/build.sh
+    cp .tmp_labscript.sh $0
+    rm -rf .tmp_labscript.sh
+    clrscr
+    printf "%s\n" "${mag}Done Updating!${end}"
+    printf '%*s\n\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+    # Running the updated script after it updates itself
+    $0
+}
 
+help() {
+        clrscr
+        echo "LazyAndroidBuilder v0.2 - Help"
+        echo "Author: Vineeth Penugonda"
+        echo -e "\nCommands Available: sync, kernel, rom, erom. reset, clean\n\n(1) sync -  Useful for syncing the ROM source code\n(2) kernel - Builds the kernel\n(3) rom - Builds the ROM\n(4) erom - Builds the Engineering ROM.\n(5) reset - Resets the build script.\n(6) clean - Cleans the build directory. Same as 'make clean && make clobber'."
+}
+
+main() {
 # echo each element in array  
 # for loop 
 for (( i=0;i<$ELEMENTS;i++)); do 
@@ -70,13 +131,13 @@ for (( i=0;i<$ELEMENTS;i++)); do
 
     if [[ ${args[${i}]} = "sync" ]]; then
         echo "Syncing Sources..."
-        repo sync --force-sync
+        #repo sync --force-sync
     fi
     
     if [[ ${args[${i}]} = "kernel" ]]; then
         clean
         source build/envsetup.sh
-        lunch lineage_c90-userdebug
+        lunch lineage_$DEVICE_NAME-userdebug
         make bootimage
     fi
 
@@ -84,25 +145,42 @@ for (( i=0;i<$ELEMENTS;i++)); do
         clean
         source build/envsetup.sh
         croot
-        brunch c90
+        brunch $DEVICE_NAME
     fi
 
     if [[ ${args[${i}]} = "erom" ]]; then
         clean
         source build/envsetup.sh
         croot
-        brunch lineage_c90-eng
+        brunch lineage_{$DEVICE_NAME}-eng
     fi
 
     if [[ ${args[${i}]} = "reset" ]]; then
-        cd $SCRIPT_DIR
-        rm -f ".firstrun.dat"
+        reset
     fi
 
-    if [[ ${args[$[i]]} = "help" ]]; then
-        clrscr
-        echo "LazyAndroidBuilder v0.1 - Help"
-        echo "Author: Vineeth Penugonda"
-        echo -e "\nCommands Available: sync, kernel, rom, erom\n\n(1) sync -  Useful for syncing the ROM source code\n(2) kernel - Builds the kernel\n(3) rom - Builds the ROM\n(4) erom - Builds the Engineering ROM."
+    if [[ ${args[${i}]} = "clean" ]]; then
+        clean
+    fi
+
+    if [[ ${args[${i}]} = "update" ]]; then
+        update_script
+    fi
+
+    if [[ ${args[${i}]} = "help" ]]; then
+        help
     fi
 done
+}
+
+if [[ $1 = "help" ]]; then
+    help
+elif [[ $1 = "reset" ]]; then
+    reset
+else
+    first_run
+fi
+
+changedir
+set_env
+main
